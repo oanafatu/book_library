ef6aeff739dbb80715e1a76f1a673afbdfe0efbd
oana fatu <oana.fatu@appliedtechnology.se>
2019-07-08 15:45:52 +0200

    Updates

---
+++ precache-manifest.b7e0d5d3b6b4d8e4560319b076cc4e5d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b173dc49f4396e258ce803ccf921287a",
+    "url": "/book_library/index.html"
+  },
+  {
+    "revision": "303ede9e3b20ea8c0a44",
+    "url": "/book_library/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e917b5a22945b9df8cb0",
+    "url": "/book_library/static/css/main.d7336446.chunk.css"
+  },
+  {
+    "revision": "303ede9e3b20ea8c0a44",
+    "url": "/book_library/static/js/2.530a58c2.chunk.js"
+  },
+  {
+    "revision": "e917b5a22945b9df8cb0",
+    "url": "/book_library/static/js/main.b14b5d45.chunk.js"
+  },
+  {
+    "revision": "38a99b414187fd749824",
+    "url": "/book_library/static/js/runtime~main.ae0956fb.js"
+  },
+  {
+    "revision": "a647578cbc9234991dc35aaa7f94a638",
+    "url": "/book_library/static/media/book_library.a647578c.png"
+  }
+]);
